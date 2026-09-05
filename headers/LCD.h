730ea54ed9867ada027e145154392de9e7dd72f3
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-05 14:24:10 +0300

    Add files via upload

---
+++ headers/LCD.h
@@ -0,0 +1,21 @@
+﻿#include "std_types.h"
+#include "DIO.h"
+#define LCD_ctrl PORTC
+#define LCD_DATA PORTD
+#define LCD_ctrlid DPORTC
+#define LCD_DATAid DPORTD
+//init
+
+void LCD_init(void);
+
+//char
+
+void LCD_SendData(u8 Data);
+
+//send command
+
+void LCD_SendCommand(u8 command);
+
+//Send String
+
+void LCD_SendString (u8 *str);
---
+++ headers/keypad.h
@@ -0,0 +1,13 @@
+﻿#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#include "std_types.h"
+#define KEYPAD_PORT DPORTA
+//first 4 pins OUTPUT , last 4 pins INPUT
+
+
+
+void KEYPAD_init(void);
+u8   KEYPAD_value(void);
+
+#endif
