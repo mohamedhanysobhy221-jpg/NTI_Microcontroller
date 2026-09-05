504a6c969b61ba79fbb2205f91160fc54b1c9347
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-05 20:37:03 +0300

    Add files via upload

---
+++ headers/STEPPER_MOTOR.h
@@ -0,0 +1,10 @@
+﻿#ifndef STEPPER_MOTOR_H_
+#define STEPPER_MOTOR_H_
+
+#include "std_types.h"
+#define MOTOR_PORT DPORTC
+void MOTOR_init(void);
+void fullstep(u8 direction);
+void halfstep(u8 direction);
+
+#endif
