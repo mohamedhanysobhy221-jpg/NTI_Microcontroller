86afe3eddc68cce1daa46085aefef97124f3e01b
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-05 14:51:01 +0300

    Add files via upload

---
+++ source/LCD.c
@@ -0,0 +1,64 @@
+﻿#include "avr/io.h"
+#include "DIO.h"
+#include "std_types.h"
+#include <util/delay.h>
+
+#define RS  0
+#define RW  1
+#define EN  2
+
+
+
+void LCD_init(void) {
+	_delay_ms(500);
+	
+	DIO_voidSetPortDirection(DPORTC, OUTPUT);
+	DIO_voidSetPortDirection(DPORTD, OUTPUT);
+	
+	_delay_ms(50);
+	
+	LCD_SendCommand(0x38);
+	_delay_us(50);
+	
+	LCD_SendCommand(0x0C);
+	_delay_us(50);
+	
+	LCD_SendCommand(0x01);
+	_delay_ms(2);
+	
+	LCD_SendCommand(0x06);
+	_delay_us(50);
+}
+
+void LCD_SendCommand(u8 command) {
+	clr_bit(PORTC, RS);
+	clr_bit(PORTC, RW);
+	
+	PORTD = command;
+	
+	set_bit(PORTC, EN);
+	_delay_us(1);
+	clr_bit(PORTC, EN);
+	
+	_delay_us(100);
+}
+
+void LCD_SendData(u8 data) {
+	set_bit(PORTC, RS);
+	clr_bit(PORTC, RW);
+	
+	PORTD = data;
+	
+	set_bit(PORTC, EN);
+	_delay_us(1);
+	clr_bit(PORTC, EN);
+	
+	_delay_us(100);
+}
+
+void LCD_SendString(u8 *str) {
+	while (*str != '\0') {
+		LCD_SendData((u8)*str);
+		str++;
+	}
+}
---
+++ source/keypad.c
@@ -0,0 +1,37 @@
+﻿#include "std_types.h"
+#include "DIO.h"
+
+void KEYPAD_init(void){
+	
+	DIO_voidSetPinDirection(DPORTC,PIN0,OUTPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN1,OUTPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN2,OUTPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN3,OUTPUT);
+	
+	DIO_voidSetPinDirection(DPORTC,PIN4,INPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN5,INPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN6,INPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN7,INPUT);
+
+	DIO_voidSetPinValue(DPORTC,PIN4,HIGH);
+	DIO_voidSetPinValue(DPORTC,PIN5,HIGH);
+	DIO_voidSetPinValue(DPORTC,PIN6,HIGH);
+	DIO_voidSetPinValue(DPORTC,PIN7,HIGH);
+}
+u8 arr[4][4] = {{7,8,9,'÷'},{4,5,6,'×'},{1,2,3,'-'},{'o',0,'=','+'}};
+u8 KEYPAD_value(void){
+	u8 Value = 0b11111111;
+	
+	for(int i = 0;i<4;i++){
+		DIO_voidSetPinValue(DPORTC,i,LOW);
+		for(int j = 0;j<4;j++){
+			if(DIO_u8GetPinValue(DPORTC,j+4)==0){
+				Value = arr[i][j];
+			}
+			
+			
+		}
+		DIO_voidSetPinValue(DPORTC,i,HIGH);
+	}
+	return Value;
+}
