45845eb97032d85222d91c40583b824688d24ae0
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-07 13:26:49 +0300

    Add files via upload

---
+++ labs/LEC4LAB1.c
@@ -0,0 +1,30 @@
+#include "DIO.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+int main(void)
+{
+    DIO_voidSetPinDirection(DPORTC,PIN0,OUTPUT);
+	DIO_voidSetPinDirection(DPORTC,PIN1,OUTPUT);
+	
+	DIO_voidSetPinDirection(DPORTD,PIN2,INPUT);
+	DIO_voidSetPinDirection(DPORTD,PIN3,INPUT);
+	DIO_voidSetPinValue(DPORTD,PIN2,HIGH);
+	DIO_voidSetPinValue(DPORTD,PIN3,HIGH);
+	initExternalInterupts();
+    while (1) 
+    {
+		
+    }
+	
+
+}
+ISR(INT0_vect)
+	{
+		toggle_bit(PORTC,PIN0);
+	}
+
+	ISR(INT1_vect)
+	{
+		toggle_bit(PORTC,PIN1);
+	}
---
+++ labs/LEC4LAB2.c
@@ -0,0 +1,30 @@
+#include "DIO.h"
+#include <avr/interrupt.h>
+#include "ISR.h"
+#include <avr/io.h>
+
+
+int main(void)
+{	
+	initExternalInterupts();
+    DIO_voidSetPortDirection(DPORTA,OUTPUT);
+	DIO_voidSetPinDirection(DPORTD,PIN2,INPUT);
+	DIO_voidSetPinValue(DPORTD,PIN2,OUTPUT);
+	DIO_voidSetPortBinaryValue(DPORTA,0b10101010);
+	
+    while (1) 
+    {
+		
+    }
+}
+ISR(INT0_vect)
+{	
+if(DIO_u8GetPinValue(DPORTD,PIN2) == 0){
+	DIO_voidSetPortBinaryValue(DPORTA,0b01010101);}
+else
+DIO_voidSetPortBinaryValue(DPORTA,0b10101010);
+	
+}
+
+
+
