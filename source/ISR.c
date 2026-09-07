138cfebcda3221e6a604ed8de960f34d6424e411
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-07 13:25:16 +0300

    Add files via upload

---
+++ source/ISR.c
@@ -0,0 +1,19 @@
+﻿#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "ISR.h"
+#include "DIO.h"
+
+void initExternalInterupts(void)
+{
+    
+   set_bit(MCUCR, ISC00);
+    clr_bit(MCUCR, ISC01);
+    set_bit(MCUCR, ISC10);
+    clr_bit(MCUCR, ISC11);
+ 
+    set_bit(GICR, INT0);
+    set_bit(GICR, INT1);
+    
+
+    set_bit(SREG, 7);
+}
