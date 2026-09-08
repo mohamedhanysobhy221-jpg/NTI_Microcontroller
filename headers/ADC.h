b5dd84daf80202da21ed797892e764ad1988a8eb
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-08 13:34:32 +0300

    Add files via upload

---
+++ headers/ADC.h
@@ -0,0 +1,14 @@
+﻿#ifndef ADC_H_
+#define ADC_H_
+
+#define ADMUX_Reg 	*((volatile u8*)0x27) 	// ADC Multiplexer Register
+#define ADCSRA_Reg 	*((volatile u8*)0x26) 	// ADC Control And Status Register
+#define ADCH_Reg 	*((volatile u8*)0x25) 		// ADC High Register
+#define ADCL_Reg 	*((volatile u8*)0x24) 		// ADC Low Register
+
+
+extern void ADC_Init (void); // ADC Initialization And Enable
+extern u16 ADC_Read (u8); // Read From The ADC Channel
+
+
+#endif /* ADC_INITIALIZATION_H_ */
