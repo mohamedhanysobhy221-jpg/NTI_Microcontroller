be4da6858154a25b0b83ce1bb328c92301fd0e3e
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-09 16:26:08 +0300

    Add files via upload

---
+++ source/TIMER0.c
@@ -0,0 +1,35 @@
+﻿#include "BITMATH.h"
+#include "TIMER0.h"
+#define Normal_mode 0
+#define CTC_mode 1
+void TIMER0_voidInit(void)
+{
+if(Normal_mode){	
+	
+	CLR_BIT(TCCR0, 6); 
+	CLR_BIT(TCCR0, 3); 
+
+	
+	CLR_BIT(TCCR0, 2); 
+	SET_BIT(TCCR0, 1);
+	CLR_BIT(TCCR0, 0);
+	}
+else if(CTC_mode){
+	CLR_BIT(TCCR0, 6);
+	SET_BIT(TCCR0, 3);
+	
+	SET_BIT(TIMSK, 1);
+	CLR_BIT(TCCR0, 2);
+	SET_BIT(TCCR0, 1);
+	CLR_BIT(TCCR0, 0); 
+}
+}
+
+void TIMER0_voidSetTimerValue(u8 Value)
+{
+	TCNT0 = Value;
+}
+void TIMER0_voidSetCompare(u8 Val)
+{
+	OCR0 = Val;
+}
---
+++ source/TIMER1.c
@@ -0,0 +1,28 @@
+﻿#include "BITMATH.h"
+#include "TIMER1.h"
+#include "std_types.h"
+
+
+
+void PWM1_voidInit(void)
+{
+	CLR_BIT(TCCR1A_REG, 0); 
+	SET_BIT(TCCR1A_REG, 1); 
+	SET_BIT(TCCR1B_REG, 3);
+	SET_BIT(TCCR1B_REG, 4); 
+
+	
+	CLR_BIT(TCCR1A_REG, 6); 
+	SET_BIT(TCCR1A_REG, 7); 
+
+	CLR_BIT(TCCR1B_REG, 0); 
+	SET_BIT(TCCR1B_REG, 1);
+	CLR_BIT(TCCR1B_REG, 2);
+
+	ICR1_REG = 2500;
+}
+void Servo_voidSetAngle(u8 Angle)
+{
+	
+	OCR1A_REG = 125 + (((u32)Angle * 125UL) / 180UL);
+}
