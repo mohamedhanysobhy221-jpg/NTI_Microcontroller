64ebc39bfbf9a6aab3731261b8fcbb39d145758c
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-09 22:55:48 +0300

    Add files via upload

---
+++ headers/SPI.h
@@ -0,0 +1,13 @@
+﻿#ifndef SPI_H_
+#define SPI_H_
+
+#include "std_types.h"
+#define SPCR_REG   (*((volatile u8*)0x2D))
+#define SPSR_REG   (*((volatile u8*)0x2E))
+#define SPDR_REG   (*((volatile u8*)0x2F))
+void SPI_InitMaster(void);
+void SPI_InitSlave(void);
+u8 SPI_GetChar(void);
+void SPI_SendChar(u8 data);
+
+#endif
---
+++ headers/UART.h
@@ -0,0 +1,16 @@
+﻿#ifndef UART_H_
+#define UART_H_
+
+#include "std_types.h"
+#define UDR    (*((volatile u8*)0x2C))
+#define UCSRA  (*((volatile u8*)0x2B))
+#define UCSRB  (*((volatile u8*)0x2A))
+#define UCSRC  (*((volatile u8*)0x40))
+#define UBRRL  (*((volatile u8*)0x29))
+#define UBRRH  (*((volatile u8*)0x40))
+
+void UART_Init(void);
+void UART_SendChar(u8 data);
+u8 UART_GetChar(void);
+
+#endif
