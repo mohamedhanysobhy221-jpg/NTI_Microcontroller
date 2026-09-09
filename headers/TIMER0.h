501591ba14522d09aa7bf1a092643deb3715152b
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-09 16:25:39 +0300

    Add files via upload

---
+++ headers/TIMER0.h
@@ -0,0 +1,17 @@
+﻿#ifndef TIMER0_H_
+#define TIMER0_H_
+
+#include "std_types.h"
+
+// Register Definitions
+#define TCCR0   *((volatile u8*)0x53)
+#define TCNT0   *((volatile u8*)0x52)
+#define OCR0    *((volatile u8*)0x5C)
+#define TIMSK   *((volatile u8*)0x59)
+#define TIFR    *((volatile u8*)0x58)
+
+void TIMER0_voidInit(void);
+void TIMER0_voidSetCompare(u8 Val);
+void TIMER0_voidSetTimerValue(u8 Value);
+
+#endif
---
+++ headers/TIMER1.h
@@ -0,0 +1,8 @@
+﻿#include "std_types.h"
+
+#define TCCR1A_REG   (*((volatile u8*)0x4F))
+#define TCCR1B_REG   (*((volatile u8*)0x4E))
+#define ICR1_REG     (*((volatile u16*)0x46))
+#define OCR1A_REG    (*((volatile u16*)0x4A))
+void PWM1_voidInit(void);
+void Servo_voidSetAngle(u8 Angle);
