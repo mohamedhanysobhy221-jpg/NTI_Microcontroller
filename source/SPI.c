5ca17a46822fbac25564fa396ac0da60a4bbfa1e
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-09 22:56:25 +0300

    Add files via upload

---
+++ source/SPI.c
@@ -0,0 +1,45 @@
+﻿#include "STD_TYPES.h"
+#include "BITMATH.h"
+#include "SPI.h"
+
+
+void SPI_InitMaster(void)
+{
+	
+	SET_BIT(SPCR_REG, 6);
+
+	CLR_BIT(SPCR_REG, 5);
+
+	SET_BIT(SPCR_REG, 4);
+
+	
+	CLR_BIT(SPCR_REG, 3);
+	CLR_BIT(SPCR_REG, 2);
+
+	
+	CLR_BIT(SPCR_REG, 1);
+	SET_BIT(SPCR_REG, 0);
+}
+
+void SPI_InitSlave(void)
+{	
+	SET_BIT(SPCR_REG, 6);
+
+	CLR_BIT(SPCR_REG, 5);
+
+	CLR_BIT(SPCR_REG, 4);
+}
+
+void SPI_SendChar(u8 data)
+{
+	SPDR_REG = data;
+
+	while (GET_BIT(SPSR_REG, 7) == 0);
+}
+
+u8 SPI_GetChar(void)
+{
+	while (GET_BIT(SPSR_REG, 7) == 0);
+	
+	return SPDR_REG;
+}
---
+++ source/UART.c
@@ -0,0 +1,40 @@
+﻿#include "UART.h"
+#include "std_types.h"
+#include "BITMATH.h"
+void UART_Init(void)
+{
+	
+	UBRRL = 6;
+	UBRRH = 0;
+
+	
+	SET_BIT(UCSRB, 4); 
+	SET_BIT(UCSRB, 3); 
+
+	
+	//UCSRC = (1 << 7) | (1 << 2) | (1 << 1);
+	SET_BIT(UCSRC,7);
+	CLR_BIT(UCSRC,6);
+	CLR_BIT(UCSRC,4);
+	CLR_BIT(UCSRC,5);
+	CLR_BIT(UCSRC,3);
+	SET_BIT(UCSRC,1);
+	SET_BIT(UCSRC,2);
+	CLR_BIT(UCSRB,2);
+}
+
+void UART_SendChar(u8 data)
+{
+	while (GET_BIT(UCSRA, 5) == 0);
+	
+	
+	UDR = data;
+}
+
+u8 UART_GetChar(void)
+{
+
+	while (GET_BIT(UCSRA, 7) == 0);
+	
+	return UDR;
+}
