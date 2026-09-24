36224e83b72104950b95d7876c833b2aa5c6e6c7
mohamedhanysobhy221-jpg <mohamedhanysobhy221@gmail.com>
2026-09-24 18:31:55 +0300

    Add files via upload

---
+++ Smart home2.c
@@ -0,0 +1,33 @@
+#define F_CPU 8000000UL
+
+#include "UART_int.h"
+#include "DIO_int.h"
+#include "TIMER1_int.h"
+#include "Std_Types.h"
+
+int main(void)
+{	
+	DIO_voidInitialization();
+	DIO_voidSetPinDirection(DPORTD, PIN5, OUTPUT);
+
+	
+	DIO_voidSetPinDirection(DPORTD, PIN0, INPUT);
+	DIO_voidSetPinDirection(DPORTD, PIN1, OUTPUT);
+	UART_voidInit();
+	TIMER1_voidInitServo();
+	u8 received_data;
+  
+    while (1) 
+    {
+		received_data = UART_u8Receive();
+		if (received_data == '1')
+		{
+			TIMER1_voidSetServoAngle(90);
+		}
+		if (received_data == '0')
+		{
+			TIMER1_voidSetServoAngle(0);
+		}
+    }
+}
+
